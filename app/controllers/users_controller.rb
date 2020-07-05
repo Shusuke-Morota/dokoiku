7573b5d83bf1dde83b5553d115992699aa007c08
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-05 21:36:34 +0900

    [Add: header/logo/flash message]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+  	@user = current_user
+  end
+
+  def update
+  	@user = current_user
+  	if @user.update(user_params)
+  		redirect_to user_path, notice: "ユーザー情報を更新しました。"
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  end
+
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :email)
+  end
+end
