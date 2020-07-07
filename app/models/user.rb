268fb646bcb856e7ac690d198b494a03ae5c7e38
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-07 22:17:14 +0900

    [Add: comment & user/index]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true
-         attachment :profile_image
-         has_many :articles, dependent: :destroy
-         has_many :favorites, dependent: :destroy
+  attachment :profile_image
+  validates :name, presence: true
+  has_many :articles, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
-         def already_favorited?(article)
-         	self.favorites.exists?(article_id: article.id)
+  def already_favorited?(article)
+    self.favorites.exists?(article_id: article.id)
          	#selfにはcurrent_userが入る。current_userに結びついているいいねの中でarticle_idが今いいねしようとしているarticle.idが存在しているか
-         end
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_084840) do
+ActiveRecord::Schema.define(version: 2020_07_07_062924) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_07_06_084840) do
     t.string "image_id"
     t.integer "user_id"
     t.string "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "article_id"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
