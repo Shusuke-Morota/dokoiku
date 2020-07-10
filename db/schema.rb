ef648cb8a702ddae668db51f088afeec563bbf33
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-10 21:36:45 +0900

    [Add: follow]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_062924) do
+ActiveRecord::Schema.define(version: 2020_07_10_113335) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_07_07_062924) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
