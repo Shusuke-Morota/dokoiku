4e52abbb7a86d056055127919fcae83cbdca464c
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-20 17:37:17 +0900

    [Add] views / top / slick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,15 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+$(function() {
+	$('.slider').slick({
+		fade: true,
+		speed: 3000,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		prevArrow: '<img src="前への矢印画像のパス" class="slide-arrow prev-arrow">',
+		nextArrow: '<img src="次への矢印画像のパス" class="slide-arrow next-arrow">'
+	});
+});
+
 
