a51ed7312008d1b8b84b0ffefbe604878526fe3e
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-21 18:32:01 +0900

    [Add:] mutiple image

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_105404) do
+ActiveRecord::Schema.define(version: 2020_07_21_061957) do
+
+  create_table "article_images", force: :cascade do |t|
+    t.integer "article_id"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "image_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
