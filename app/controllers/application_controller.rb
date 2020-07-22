444372c71a56f7b6a0767384032362199c63bb9e
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-23 00:29:55 +0900

    [Add:]  search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,15 @@ class ApplicationController < ActionController::Base
     	new_user_session_path
     end
 
+    def set_search
+        @search = Product.ransack(params[:q]) #ransackの検索メソッド
+        @search_products = @search.result(distinct: true).order(created_at: "DESC").includes(:user).page(params[:page]).per(5) # productsの検索結果一覧 
+        # 最終的に、@search_productsを検索結果画面（例：search.html.haml）に挿入します。
+        # 検索結果の一覧：  @search_products = @search.result.order(created_at: "DESC")
+        # distinct: trueは検索結果のレコード重複しないようにします。
+        # ページネーション:  .includes(:user).page(params[:page]).per(5
+    end
+
 	protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
