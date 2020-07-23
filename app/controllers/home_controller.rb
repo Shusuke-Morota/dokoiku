4d7b4edabc01450ed018ff2b31fc523dd4149b81
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-23 12:58:35 +0900

    [Update:]  top / search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def top
   	@categories = Category.all
-  	@articles = Article.all.order(id: "DESC")
   	@users = User.all
   	@all_ranks = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
+  	@q = Article.ransack(params[:q])
+  	@articles = @q.result(distinct: true).order(id: "DESC")
   end
 end
