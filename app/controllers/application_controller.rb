749452005e4126b5e0a54c0d0c8af5dcd5b5086d
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-25 19:02:55 +0900

    [Add:]  more / comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
-    	user_path(current_user)
+    	root_path
     end
 
     def after_sign_up_path_for(resource)
-    	user_path(current_user)
+    	root_path
     end
 
     def after_sign_out_path_for(resource_or_scope)
@@ -15,11 +15,7 @@ class ApplicationController < ActionController::Base
 
     def set_search
         @search = Product.ransack(params[:q]) #ransackの検索メソッド
-        @search_products = @search.result(distinct: true).order(created_at: "DESC").includes(:user).page(params[:page]).per(5) # productsの検索結果一覧 
-        # 最終的に、@search_productsを検索結果画面（例：search.html.haml）に挿入します。
-        # 検索結果の一覧：  @search_products = @search.result.order(created_at: "DESC")
-        # distinct: trueは検索結果のレコード重複しないようにします。
-        # ページネーション:  .includes(:user).page(params[:page]).per(5
+        @search_products = @search.result(distinct: true).order(created_at: "DESC").includes(:user).page(params[:page]).per(5)
     end
 
 	protected
