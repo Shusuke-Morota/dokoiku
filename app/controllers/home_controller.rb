6dfa69d74ecddeb05bce62e5c98c2a255018bd36
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-07-29 20:46:29 +0900

    [Add:]  top / pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
   	@users = User.all
   	@all_ranks = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(5).pluck(:article_id))
   	@q = Article.ransack(params[:q])
-    @articles = @q.result.page(params[:page]).per(10).order(id: "DESC")
+    @articles = @q.result.page(params[:page]).per(10).order(id: "DESC").all.page(params[:page]).per(5)
   end
 end
